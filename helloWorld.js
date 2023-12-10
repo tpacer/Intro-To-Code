db8938910e09689d4bf5e9135ec8d9a913e0362b
tpacer <54083928+tpacer@users.noreply.github.com>
2023-12-10 11:37:05 -0600

    Add files via upload

---
+++ helloWorld.js
@@ -0,0 +1,26 @@
+function setup() 
+{
+  createCanvas(500,500);
+  background("blue");
+}
+
+function draw() 
+{
+   fill("white");
+   text("Hello World", 250,250);
+
+   text("Goodbye World", 250,280);
+}
+
+
+
+
+//text("odoyle rules", 250,250);
+//text("intro to coding", 10,300);  //this is the greatest code ever 
+
+
+
+
+//textSize(32); 
+//text("odoyle rules", 10,200);
+//text("intro to coding", 10,300);  //this is the greatest code ever 
